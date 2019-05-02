d6d59f4bee558bb6089350e04fa3040a2d90bc57
Victor Wang <victor.wang@open.edu.au>
2019-05-03 00:46:05 +1000

    first element

--- force-app/main/default/lwc/ozLottoGenerator/ozLottoGenerator.js
+++ force-app/main/default/lwc/ozLottoGenerator/ozLottoGenerator.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class OzLottoGenerator extends LightningElement {}
+export default class OzLottoGenerator extends LightningElement {
+    @track inputDrawNumber = "enter a number";
+
+    //TODO:
+    checkType(event) {
+        let inputDrawNumber = event.target.value;
+        console.log("entered draw number is " + inputDrawNumber);
+        //reportValidity
+    }
+
+
+}
