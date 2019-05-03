cc0fb771c5a3ce21c58161673845420b70e5ceef
Victor Wang <victor.wang@open.edu.au>
2019-05-03 18:56:22 +1000

    button commit

--- force-app/main/default/lwc/ozLottoGenerator/ozLottoGenerator.js
+++ force-app/main/default/lwc/ozLottoGenerator/ozLottoGenerator.js
@@ -2,12 +2,25 @@ import { LightningElement, track } from 'lwc';
 
 export default class OzLottoGenerator extends LightningElement {
     @track inputDrawNumber = "enter a number";
+    variant = "brand";
 
     //TODO:
     checkType(event) {
         let inputDrawNumber = event.target.value;
         console.log("entered draw number is " + inputDrawNumber);
         //reportValidity
+    }
+
+    handleToggleClick() {
+        console.log("variant is " + this.variant);
+
+        let variant = this.variant;
+
+        if(variant === 'brand') {
+            this.variant = 'Neutral';
+        } else {
+            this.variant = 'brand';
+        }
     }
 
 
