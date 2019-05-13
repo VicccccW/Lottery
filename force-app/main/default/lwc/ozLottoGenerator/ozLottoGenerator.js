3a7fc9eaa55754fe6a085412924e13fb48e5c819
Victor Wang <victor.wang@open.edu.au>
2019-05-14 00:11:05 +1000

    lucky ball commit

--- force-app/main/default/lwc/ozLottoGenerator/ozLottoGenerator.js
+++ force-app/main/default/lwc/ozLottoGenerator/ozLottoGenerator.js
@@ -3,8 +3,9 @@ import { LightningElement, track } from 'lwc';
 export default class OzLottoGenerator extends LightningElement {
     @track drawNumber;
     @track ballPool = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
-    @track luckyBalls = ["1", "2", "3", "4", "5", "6", "7"];
+    @track luckyBalls = []; //TODO: add if ture, false in html
     allBalls = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
+    //allBalls retrieve from Salesforce apex
 
     //TODO:
     checkType(event) {
@@ -13,6 +14,7 @@ export default class OzLottoGenerator extends LightningElement {
         //reportValidity
     }
 
+    //this handleReset() is different from the button.handleReset();
     handleReset() {
         //clear input field 
         const inputElement = this.template.querySelector('lightning-input');
@@ -20,54 +22,65 @@ export default class OzLottoGenerator extends LightningElement {
 
         //reset property
         this.ballPool = this.allBalls;
-        //this.exclusion = [];
-        this.luckBalls = [];
+        this.luckyBalls = [];
 
         //reset button
         const buttons = this.template.querySelectorAll('c-ball-button');
-        buttons.forEach(button => {
-            button.handleReset();
-        });
-
-        //reset generated balls
-
+        buttons.forEach(button => button.handleReset());
     }
 
     handleGenerator() {
-        if(this.ballPool.length() > 6) {
-            this.luckBalls = [];
-            
-            if() {
-                this.luckBalls.push(this.ballPool.getRandomIndex());
-            }
+        if(this.ballPool.length > 6) {
+            this.luckyBalls = [];
+
+            let indexArr = this.getRandomIndexArray(this.ballPool.length);
+
+            indexArr.forEach(element => this.luckyBalls.push(this.ballPool[element]));
         } else {
-            console.log("generator error");
+            console.log("handleGenerator error");
         }
     }
 
-    //error handling, if null/ undefined
-    getRandomIndex() {
-        return Math.floor(Math.random() * this.ballPool.length());
+    //get a random array for lucky ball array index
+    getRandomIndexArray(ballPoolLength) {
+        const indexArr = [];
+
+        while(indexArr.length < 7){
+            let r = Math.floor(Math.random() * ballPoolLength);
+
+            if(indexArr.indexOf(r) === -1) {
+                indexArr.push(r);
+            }
+        }
+
+        return indexArr;
     }
 
     addBallHandler(event) {
         const buttonValue = event.detail;
 
-        if(!this.ballPool.includesp(buttonValue)) {
+        if(!this.ballPool.includes(buttonValue)) {
             this.ballPool.push(buttonValue);
-
-            console.log("ballPool -->" + this.ballPool);
         }
         //this.exclusion = this.ballPool.filter(element => !this.allBalls.includes(element));
     }
 
+
+    //TODO: if there is only 7 button left, prevent user from remove more button.
     removeBallHandler(event) {
         const buttonValue = event.detail;
 
         if(this.ballPool.includes(buttonValue)){
             this.ballPool.splice(this.ballPool.indexOf(buttonValue),1);
-
-            console.log("ballPool -->" + this.ballPool);
         }
     }
+
+    //TODO: logic for 2 complementry balls
+
+    //TODO: css color for green and yellow balls
+
+    //TODO: a Save button to save generated records to Salesforce, a custom objects
+
+    //TODO: similar to OUA's component, a collaspe
+
 }
